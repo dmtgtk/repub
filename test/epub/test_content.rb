fd957641d3b5e1456a022be156b4ddf729faccb1
Dmitri Goutnik <dg@invisiblellama.net>
2009-06-16 13:20:40 +0400

    fixed tests to pass ruby-1.8.6 on win

--- test/epub/test_content.rb
+++ test/epub/test_content.rb
@@ -13,13 +13,13 @@ class TestContent < Test::Unit::TestCase
     # manifest was created
     assert_not_nil(doc.search('manifest'))
     # has exactly one item
-    assert_equal(1, doc.search('manifest/item').count)
+    assert_equal(1, doc.search('manifest/item').size)
     # and item is ncx
     assert_equal('ncx', doc.search('manifest/item')[0][:id])
     # spine was created
     assert_not_nil(doc.search('spine'))
     # and is empty
-    assert_equal(0, doc.search('spine/item').count)
+    assert_equal(0, doc.search('spine/item').size)
   end
 
   def test_manifest
@@ -40,15 +40,15 @@ class TestContent < Test::Unit::TestCase
     # manifest was created
     assert_not_nil(doc.search('manifest'))
     # has 2 stylesheets
-    assert_equal(2, doc.search('manifest/item[@media-type = "text/css"]').count)
+    assert_equal(2, doc.search('manifest/item[@media-type = "text/css"]').size)
     # and 2 jpegs
-    assert_equal(2, doc.search('manifest/item[@media-type = "image/jpeg"]').count)
+    assert_equal(2, doc.search('manifest/item[@media-type = "image/jpeg"]').size)
     # and 1 png
-    assert_equal(1, doc.search('manifest/item[@media-type = "image/png"]').count)
+    assert_equal(1, doc.search('manifest/item[@media-type = "image/png"]').size)
     # spine was created
     assert_not_nil(doc.search('spine'))
     # and has 3 html items
-    assert_equal(3, doc.search('spine/itemref').count)
+    assert_equal(3, doc.search('spine/itemref').size)
     # check that order is as inserted and ids are correct
     assert_equal('intro', doc.search('spine/itemref')[0]['idref'])
     assert_equal('glossary', doc.search('spine/itemref')[2]['idref'])
