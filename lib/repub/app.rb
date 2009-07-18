3839bffd2b1fbcb6b95d0a9a615c77921ca6eb93
Dmitri Goutnik <dg@invisiblellama.net>
2009-07-18 17:43:57 +0400

    exception handling

--- lib/repub/app.rb
+++ lib/repub/app.rb
@@ -39,10 +39,10 @@ module Repub
       
       Launchy::Browser.run(builder.document_path) if options[:browser]
     
-    # rescue RuntimeError => ex
-    #   log.fatal "** ERROR: #{ex.to_s}"
-    rescue Exception => ex
+    rescue RuntimeError => ex
       log.fatal "** ERROR: #{ex.to_s}"
+    rescue Exception => ex
+      log.fatal "** ERROR: #{ex.to_s}" unless ex.is_a? SystemExit
     end
   
   end
